cf36793658dc981e16db15426e5bf0d9584c4b30
Rifat-Rashid <s-RRashid@lwsd.org>
2015-08-07 15:34:43 -0700

    comit

--- app/src/main/java/com/example/rifatrashid/loworbitioncannon/MainActivity.java
+++ app/src/main/java/com/example/rifatrashid/loworbitioncannon/MainActivity.java
@@ -1,37 +1,24 @@
 package com.example.rifatrashid.loworbitioncannon;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static UDPpacket udpPacket;
+    public static long numberOfPacketsSent = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void initialize() throws MalformedURLException, UnknownHostException {
+        InetAddress address = InetAddress.getByName(new URL("xxxx").getHost());
     }
 }

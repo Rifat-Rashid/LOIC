94f2f6d6610029d0439ae668df1c23d287a46e34
Rifat-Rashid <s-RRashid@lwsd.org>
2015-08-07 21:53:27 -0700

    comit

--- app/src/main/java/com/example/rifatrashid/loworbitioncannon/MainActivity.java
+++ app/src/main/java/com/example/rifatrashid/loworbitioncannon/MainActivity.java
@@ -1,21 +1,25 @@
 package com.example.rifatrashid.loworbitioncannon;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
 
 import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.UnknownHostException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
 
     public static UDPpacket udpPacket;
     public static long numberOfPacketsSent = 0;
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getActionBar().setElevation(0);
     }
 
     public void initialize() throws MalformedURLException, UnknownHostException {

8331dac66485f8130a51c255b9e421474cdc8c58
Rifat-Rashid <s-RRashid@lwsd.org>
2015-08-12 16:36:32 -0700

    comit

---
+++ app/src/main/java/com/example/rifatrashid/loworbitioncannon/HTTPpacket.java
@@ -0,0 +1,7 @@
+package com.example.rifatrashid.loworbitioncannon;
+
+/**
+ * Created by Rifat Rashid on 8/12/2015.
+ */
+public class HTTPpacket {
+}
---
+++ app/src/main/java/com/example/rifatrashid/loworbitioncannon/TCPpacket.java
@@ -0,0 +1,49 @@
+package com.example.rifatrashid.loworbitioncannon;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+/**
+ * Created by Rifat Rashid on 8/12/2015.
+ */
+public class TCPpacket implements Runnable {
+
+    private InetAddress IPAddress;
+    private byte[] sendData;
+    private DatagramPacket sendPacket;
+    private int port;
+    private boolean isRunning = false;
+    private boolean isReachable = true;
+    public static int count = 0;
+    public static long sTime = 0;
+
+    public TCPpacket(InetAddress IPAddress, byte[] sendData, int port) throws IOException {
+        this.IPAddress = IPAddress;
+        this.sendData = sendData;
+        this.port = port;
+    }
+    @Override
+    public void run() {
+        count = 0;
+        sTime = System.currentTimeMillis();
+        Socket sock;
+        while(managerClass.firing){
+            try{
+                sock = new Socket();
+                sock.connect(new InetSocketAddress(IPAddress, port), 10);
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(sock.getOutputStream());
+                outputStreamWriter.write(sendData.toString());
+                outputStreamWriter.close();
+                sock.close();
+                count++;
+                Thread.sleep(10);
+            }catch (Exception io){
+                io.printStackTrace();
+            }
+        }
+    }
+}

ee742527f8c3848bcf1bdd8e66f4f96929692b3a
Rifat-Rashid <s-RRashid@lwsd.org>
2015-08-12 17:28:12 -0700

    comit

--- app/src/main/java/com/example/rifatrashid/loworbitioncannon/MainActivity.java
+++ app/src/main/java/com/example/rifatrashid/loworbitioncannon/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rifatrashid.loworbitioncannon;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.net.InetAddress;
 import java.net.URL;
@@ -38,6 +40,7 @@ public class MainActivity extends ActionBarActivity implements CompoundButton.On
     private int methodType = 1;
     private TextView elapsedTimeText;
     private EditText numberOfThreadsText;
+    private boolean gotIP = false;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,10 +77,12 @@ public class MainActivity extends ActionBarActivity implements CompoundButton.On
                 if (!usingURL) {
                     tempURL = ipTextBox.getText().toString();
                     urlTextView.setText(tempURL);
+                    gotIP = true;
                 } else {
                     try {
                         address = InetAddress.getByName(new URL(tempURL).getHost());
                         urlTextView.setText(address.getHostAddress().toString());
+                        gotIP = true;
                     } catch (Exception e) {
                         //Error
                         e.printStackTrace();
@@ -91,85 +96,106 @@ public class MainActivity extends ActionBarActivity implements CompoundButton.On
             @Override
             public void onClick(View v) {
                 if (!managerClass.firing) {
-                    if (portText.getText() != null) {
-                        if (Pattern.matches("[a-zA-Z]+", portText.getText()) == false) {
-                            PORT = Integer.parseInt(String.valueOf(portText.getText()));
-                            if (numberOfThreadsText.getText() != null) {
-                                if (Pattern.matches("[a-zA-Z]+", numberOfThreadsText.getText()) == false) {
-                                    THREADS = Integer.parseInt(String.valueOf(numberOfThreadsText.getText()));
-                                } else {
-                                    //Basic number of threads
-                                    THREADS = 10;
-                                }
-                                //Insert code block here !
+                    if(gotIP) {
+                        if (portText.getText() != null) {
+                            if (Pattern.matches("[a-zA-Z]+", portText.getText()) == false) {
                                 try {
-                                    InetAddress realAddress = InetAddress.getByName(address.getHostAddress());
-                                    services.start(realAddress, PORT, THREADS, sendingBytes, methodType);
-                                    fireButton.setText("Stop");
-                                    Runnable r = new Runnable() {
-                                        public void run() {
-                                            while (managerClass.firing) {
-                                                switch (methodType) {
-                                                    case 1:
-                                                        //UDP
-                                                        MainActivity.this.runOnUiThread(new Runnable() {
-                                                            @Override
-                                                            public void run() {
-                                                                try {
-                                                                    numberOfPacketSentText.setText(String.valueOf(UDPpacket.count));
-                                                                    packetsPerSecondText.setText(String.valueOf(Math.round(UDPpacket.count / ((System.currentTimeMillis() - UDPpacket.sTime) / 1000.0))));
-                                                                    elapsedTimeText.setText("Elapsed Time: " + (System.currentTimeMillis() - UDPpacket.sTime) / 1000.0 + "s");
-                                                                } catch (Exception e) {
-                                                                    e.printStackTrace();
+                                    PORT = Integer.parseInt(String.valueOf(portText.getText()));
+                                }catch (Exception e){
+                                    e.printStackTrace();
+                                }
+                                if (numberOfThreadsText.getText() != null) {
+                                    if (Pattern.matches("[a-zA-Z]+", numberOfThreadsText.getText()) == false) {
+                                        try {
+                                            THREADS = Integer.parseInt(String.valueOf(numberOfThreadsText.getText()));
+                                        }catch (Exception e){
+                                            e.printStackTrace();
+                                        }
+                                    } else {
+                                        //Basic number of threads
+                                        THREADS = 10;
+                                    }
+                                    //Insert code block here !
+                                    try {
+                                        InetAddress realAddress = InetAddress.getByName(address.getHostAddress());
+                                        services.start(realAddress, PORT, THREADS, sendingBytes, methodType);
+                                        fireButton.setText("Stop");
+                                        Runnable r = new Runnable() {
+                                            public void run() {
+                                                while (managerClass.firing) {
+                                                    switch (methodType) {
+                                                        case 1:
+                                                            //UDP
+                                                            MainActivity.this.runOnUiThread(new Runnable() {
+                                                                @Override
+                                                                public void run() {
+                                                                    try {
+                                                                        numberOfPacketSentText.setText(String.valueOf(UDPpacket.count));
+                                                                        packetsPerSecondText.setText(String.valueOf(Math.round(UDPpacket.count / ((System.currentTimeMillis() - UDPpacket.sTime) / 1000.0))));
+                                                                        elapsedTimeText.setText("Elapsed Time: " + (System.currentTimeMillis() - UDPpacket.sTime) / 1000.0 + "s");
+                                                                    } catch (Exception e) {
+                                                                        e.printStackTrace();
+                                                                    }
                                                                 }
-                                                            }
-                                                        });
-                                                        break;
-                                                    case 2:
-                                                        //HTTP
-                                                        MainActivity.this.runOnUiThread(new Runnable() {
-                                                            @Override
-                                                            public void run() {
-                                                                try {
-                                                                    numberOfPacketSentText.setText(String.valueOf(HTTPpacket.count));
-                                                                    packetsPerSecondText.setText(String.valueOf(Math.round(HTTPpacket.count / ((System.currentTimeMillis() - HTTPpacket.sTime) / 1000.0))));
-                                                                    elapsedTimeText.setText("Elapsed Time: " + (System.currentTimeMillis() - HTTPpacket.sTime) / 1000.0 + "s");
-                                                                } catch (Exception e) {
-                                                                    e.printStackTrace();
+                                                            });
+                                                            break;
+                                                        case 2:
+                                                            //HTTP
+                                                            MainActivity.this.runOnUiThread(new Runnable() {
+                                                                @Override
+                                                                public void run() {
+                                                                    try {
+                                                                        numberOfPacketSentText.setText(String.valueOf(HTTPpacket.count));
+                                                                        packetsPerSecondText.setText(String.valueOf(Math.round(HTTPpacket.count / ((System.currentTimeMillis() - HTTPpacket.sTime) / 1000.0))));
+                                                                        elapsedTimeText.setText("Elapsed Time: " + (System.currentTimeMillis() - HTTPpacket.sTime) / 1000.0 + "s");
+                                                                    } catch (Exception e) {
+                                                                        e.printStackTrace();
+                                                                    }
                                                                 }
-                                                            }
-                                                        });
-                                                        break;
-                                                    case 3:
-                                                        //TCP
-                                                        MainActivity.this.runOnUiThread(new Runnable() {
-                                                            @Override
-                                                            public void run() {
-                                                                try {
-                                                                    numberOfPacketSentText.setText(String.valueOf(TCPpacket.count));
-                                                                    packetsPerSecondText.setText(String.valueOf(Math.round(TCPpacket.count / ((System.currentTimeMillis() - TCPpacket.sTime) / 1000.0))));
-                                                                    elapsedTimeText.setText("Elapsed Time: " + (System.currentTimeMillis() - TCPpacket.sTime) / 1000.0 + "s");
-                                                                } catch (Exception e) {
-                                                                    e.printStackTrace();
+                                                            });
+                                                            break;
+                                                        case 3:
+                                                            //TCP
+                                                            MainActivity.this.runOnUiThread(new Runnable() {
+                                                                @Override
+                                                                public void run() {
+                                                                    try {
+                                                                        numberOfPacketSentText.setText(String.valueOf(TCPpacket.count));
+                                                                        packetsPerSecondText.setText(String.valueOf(Math.round(TCPpacket.count / ((System.currentTimeMillis() - TCPpacket.sTime) / 1000.0))));
+                                                                        elapsedTimeText.setText("Elapsed Time: " + (System.currentTimeMillis() - TCPpacket.sTime) / 1000.0 + "s");
+                                                                    } catch (Exception e) {
+                                                                        e.printStackTrace();
+                                                                    }
                                                                 }
-                                                            }
-                                                        });
-                                                        break;
-                                                }
-                                                try {
-                                                    Thread.sleep(50);
-                                                } catch (Exception io) {
-                                                    io.printStackTrace();
+                                                            });
+                                                            break;
+                                                    }
+                                                    try {
+                                                        Thread.sleep(50);
+                                                    } catch (Exception io) {
+                                                        io.printStackTrace();
+                                                    }
                                                 }
                                             }
-                                        }
-                                    };
-                                    new Thread(r).start();
-                                } catch (Exception io) {
-
+                                        };
+                                        new Thread(r).start();
+                                    } catch (Exception io) {
+                                        io.printStackTrace();
+                                    }
                                 }
-                            }
 
+                            }
+                        }
+                    }else{
+                        try {
+                            Context context = getApplicationContext();
+                            CharSequence errorMessage = "IP Not Selected";
+                            int duration = Toast.LENGTH_LONG;
+                            Toast toast = Toast.makeText(context, errorMessage, duration);
+                            toast.show();
+                        }catch (Exception e){
+                            //Something happened here!
+                            e.printStackTrace();
                         }
                     }
                 } else {
